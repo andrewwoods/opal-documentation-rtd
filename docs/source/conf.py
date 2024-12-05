0303455629ca13c2f2d1cc0801255340c28c4904
Andrew Woods <services@awoods.net>
2024-12-05 14:02:39 -0500

    Replace tutorial config settings with mine
    Updated the project name and copyright info.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Opal'
+copyright = '2024, Andrew Woods'
+author = 'Andrew Woods'
 
 release = '0.1'
 version = '0.1.0'
